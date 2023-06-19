807df58ddf49133efd880d8b88f2bb0d13ad9264
martinapiglio <martina.piglio@gmail.com>
2023-06-19 11:18:05 +0200

    feat: api call to users table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  created() {
+    this.getUsers();
+  },
+
+  methods: {
+    getUsers() {
+      axios.get('http://127.0.0.1:8000/api/users').then(response => {
+        this.users = response.data.results;
+        console.log(response.data.results);
+      });
+    }
+  }
+}
 
 
 </script>
