4b7e38a5f34298620cbca75269fc88e8aacc98e6
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-06-19 18:04:27 +0200

    fix: filtered base on mainspec

--- src/pages/doctors/DoctorsIndex.vue
+++ src/pages/doctors/DoctorsIndex.vue
@@ -1,19 +1,20 @@
 <script>
-import axios from 'axios';
-import DoctorCard from '../../components/DoctorCard.vue';
+import axios from "axios";
+import DoctorCard from "../../components/DoctorCard.vue";
 
 export default {
-  name: 'DoctorIndex',
+  name: "DoctorIndex",
   data() {
     return {
       users: [],
       specs: [],
-      filteredSpec: '',
-    }
+      filteredSpec: "",
+      doctorsFound: false,
+    };
   },
 
   components: {
-    DoctorCard
+    DoctorCard,
   },
 
   created() {
@@ -22,38 +23,62 @@ export default {
 
   methods: {
     getUsers() {
-      axios.get('http://127.0.0.1:8000/api/users' + '?mainspec=' + this.filteredSpec).then(response => {
-        this.users = response.data.results;
-        this.specs = response.data.specs;
-
-      });
+      axios
+        .get(
+          "http://127.0.0.1:8000/api/users" + "?mainspec=" + this.filteredSpec
+        )
+        .then((response) => {
+          if (response.data.success) {
+            console.log(response.data.specs);
+            this.users = response.data.results;
+            this.specs = response.data.specs;
+            console.log(this.users);
+            console.log(this.specs);
+            this.doctorsFound = true;
+          } else {
+            this.doctorsFound = false;
+          }
+        });
     },
 
     getFilteredSpecs() {
       this.getUsers();
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
-    homepage dottori
+  homepage dottori
 
-    <form action="">
-      <select class="form-select" name="mainspec" id="mainspec" v-model="filteredSpec" @change="getFilteredSpecs">
-        <option value="">Tutte</option>
-        <option v-for="spec in specs" :value="spec.id">{{ spec.title }}</option>
-      </select>
-    </form>
+  <form action="">
+    <select
+      class="form-select"
+      name="mainspec"
+      id="mainspec"
+      v-model="filteredSpec"
+      @change="getFilteredSpecs"
+    >
+      <option value="">Tutte</option>
+      <option v-for="spec in specs" :value="spec.title">
+        {{ spec.title }}
+      </option>
+    </select>
+  </form>
 
-    <div class="container d-flex justify-content-center flex-wrap gap-3 py-5">
-
-      <div v-for="user in users">
-        <DoctorCard :doctor="user"></DoctorCard>
-      </div>
+  <div
+    v-if="doctorsFound"
+    class="container d-flex justify-content-center flex-wrap gap-3 py-5"
+  >
+    <div v-for="user in users">
+      <DoctorCard :doctor="user"></DoctorCard>
     </div>
-
+  </div>
+  <div v-else>
+    <div role="alert" class="alert alert-warning text-center">
+      Nessun dottore trovato
+    </div>
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
