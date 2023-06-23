141ebdedb8f9c84d784cc2054923cc56ea460c06
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-06-23 15:04:08 +0200

    feat: sponsorship sortment added

--- src/pages/doctors/DoctorsAdvancedResearch.vue
+++ src/pages/doctors/DoctorsAdvancedResearch.vue
@@ -15,6 +15,11 @@ export default {
       userVote: "",
       order: "",
       isLoading: true,
+      sponsoredPresent: "",
+      sponsoredUsers: [],
+
+      nonSponsoredPresent: "",
+      nonSponsoredUsers: [],
     };
   },
 
@@ -33,6 +38,20 @@ export default {
     getDescOrAsc() {
       return console.log(this.descOrAsc);
     },
+    formattedNow() {
+      const now = new Date();
+
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, "0"); // Add leading zero if needed
+      const day = String(now.getDate()).padStart(2, "0"); // Add leading zero if needed
+
+      const hours = String(now.getHours()).padStart(2, "0"); // Add leading zero if needed
+      const minutes = String(now.getMinutes()).padStart(2, "0"); // Add leading zero if needed
+      const seconds = String(now.getSeconds()).padStart(2, "0"); // Add leading zero if needed
+
+      const formattedNow = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      return formattedNow;
+    },
   },
 
   methods: {
@@ -50,6 +69,8 @@ export default {
 
             this.doctorsFound = true;
             this.isLoading = false;
+            this.getSponsoredUsers();
+            this.getNonSponsoredUsers();
           } else {
             this.doctorsFound = false;
           }
@@ -99,20 +120,63 @@ export default {
         }
       });
     },
+    getSponsoredUsers() {
+      //sponsored users array
+      let usersWithSponsorships = this.users.filter(
+        (user) =>
+          user.detail.sponsorships.length > 0 &&
+          user.detail.sponsorships[0].pivot.end_date > this.formattedNow
+      );
+      usersWithSponsorships.sort((a, b) => {
+        if (b.detail.sponsorships[0].id === a.detail.sponsorships[0].id) {
+          return (
+            new Date(b.detail.sponsorships[0].pivot.end_date) -
+            new Date(a.detail.sponsorships[0].pivot.end_date)
+          );
+        } else {
+          return b.detail.sponsorships[0].id - a.detail.sponsorships[0].id;
+        }
+      });
+
+      if (usersWithSponsorships.length > 0) {
+        this.sponsoredUsers = usersWithSponsorships;
+        this.sponsoredPresent = true;
+      } else {
+        this.sponsoredPresent = false;
+      }
+    },
+
+    getNonSponsoredUsers() {
+      //non-sponsored users array
+      let usersWithoutSponsorships = [];
+      for (let i = 0; i < this.users.length; i++) {
+        if (
+          this.users[i].detail.sponsorships.length === 0 ||
+          this.users[i].detail.sponsorships[0].pivot.end_date <
+            this.formattedNow
+        ) {
+          usersWithoutSponsorships.push(this.users[i]);
+        }
+      }
+
+      if (usersWithoutSponsorships.length > 0) {
+        this.nonSponsoredUsers = usersWithoutSponsorships;
+        this.nonSponsoredPresent = true;
+      } else {
+        this.nonSponsoredPresent = false;
+      }
+    },
   },
 };
 </script>
 
 <template>
-
   <div v-if="isLoading" class="text-center py-5">
-
     <div id="spinner-container">
       <div class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
     </div>
-
   </div>
 
   <div v-else>
@@ -129,7 +193,7 @@ export default {
         <option v-for="number in 5" :value="number">{{ number }}</option>
       </select>
     </form>
-  
+
     <div v-if="doctorsFound">
       <form action="">
         <label for="numberOfReviews">Ordina per numero di recensioni</label>
@@ -150,8 +214,30 @@ export default {
       v-if="doctorsFound"
       class="container d-flex justify-content-center flex-wrap gap-3 py-5"
     >
-      <div v-for="user in users">
-        <DoctorCard :doctor="user"></DoctorCard>
+      <h4 class="text-center mt-5">Medici in evidenza</h4>
+      <div
+        v-if="sponsoredPresent"
+        class="d-flex justify-content-center flex-wrap gap-3 py-5"
+      >
+        <div v-for="user in sponsoredUsers">
+          <DoctorCard :doctor="user"></DoctorCard>
+        </div>
+      </div>
+      <div v-else>
+        <span>Non ci sono medici in evidenza</span>
+      </div>
+
+      <h4 class="text-center mt-5">tutti gli altri medici</h4>
+      <div
+        v-if="nonSponsoredPresent"
+        class="d-flex justify-content-center flex-wrap gap-3 py-5"
+      >
+        <div v-for="user in nonSponsoredUsers">
+          <DoctorCard :doctor="user"></DoctorCard>
+        </div>
+      </div>
+      <div v-else>
+        <span>Non ci sono altri medici</span>
       </div>
     </div>
     <div v-else>
@@ -159,10 +245,7 @@ export default {
         Nessun dottore trovato
       </div>
     </div>
-
   </div>
-
-
 </template>
 
 <style></style>
