d1843fc80cebe9bede22e6ed3e4f664a32f43c97
martinapiglio <martina.piglio@gmail.com>
2023-06-26 17:07:54 +0200

    header style try

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,11 +9,22 @@ export default {
 </script>
 
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <span>bdoctors-header</span>
-        <!-- PROVVISORIO PER COLLEGAMENTO A LARAVEL -->
+  <div class="nav-cnt">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ps-4">
+      <a class="navbar-brand" href="#">logo</a>
+      <div
+        class="collapse navbar-collapse d-flex justify-content-between"
+        id="navbarNavDropdown"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+        </ul>
+
+        <a class="nav-link" href="#">Sei un dottore?</a>
         <span>
           Sei un dottore?
           <a class="btn btn-primary" href="http://127.0.0.1:8000/register">
@@ -23,50 +34,6 @@ export default {
             Fai il login
           </a>
         </span>
-        <!-- //PROVVISORIO PER COLLEGAMENTO A LARAVEL -->
-
-        <!-- <router-link
-          class="navbar-brand"
-          aria-current="page"
-          :to="{ name: 'home' }"
-          >Boolfolio</router-link
-        > -->
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <!-- <li class="nav-item">
-              <router-link
-                class="nav-link active"
-                aria-current="page"
-                :to="{ name: 'home' }"
-                >Home</router-link
-              >
-            </li>
-
-            <li class="nav-item">
-              <router-link class="nav-link" :to="{ name: 'projects.index' }"
-                >All the projects</router-link
-              >
-            </li>
-
-            <li class="nav-item">
-              <router-link class="nav-link" :to="{ name: 'about' }"
-                >Who am I?</router-link
-              >
-            </li> -->
-          </ul>
-        </div>
       </div>
     </nav>
   </div>
