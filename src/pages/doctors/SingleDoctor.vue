84a3cbdb08d7d563b3a4e23e68635151b4ce3c8f
soniagaber <soniagaber90@gmail.com>
2023-06-28 19:47:31 +0200

    layout img-details

--- src/pages/doctors/SingleDoctor.vue
+++ src/pages/doctors/SingleDoctor.vue
@@ -284,72 +284,66 @@ export default {
 
       <hr />
 
-      <img
-        :src="thumbnail"
-        style="width: 250px"
-        alt="doctor profile pic"
-        @error="thumbnailError"
-      />
+      <div class="d-flex flex-wrap gap-3">
 
-      <div>
-        <label for="__mainspec"
-          ><strong>Specializzazione principale: </strong></label
-        >
-        <div id="__mainspec">
-          {{ doctor.mainspec }}
+        <img :src="thumbnail" style="width: 250px; object-fit:cover" alt="doctor profile pic" @error="thumbnailError" />
+
+        <div id="details-container">
+          <div>
+            <label for="__mainspec"><strong>Specializzazione principale: </strong></label>
+            <div id="__mainspec">
+              {{ doctor.mainspec }}
+            </div>
+          </div>
+
+          <div>
+            <label for="__other-specs"><strong>Altre specializzazioni: </strong></label>
+            <ul id="__other-specs">
+              <li v-for="spec in doctor.detail.specs">
+                {{ spec.title }}
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <label for="__description"><strong>Descrizione </strong></label>
+
+            <p id="__description">
+              {{ doctor.description }}
+            </p>
+          </div>
+
+          <div v-if="doctor.detail.curriculum" class="my-4">
+            <a :href="curriculum" target="_blank" class="btn btn-primary">Vedi il CV</a>
+          </div>
+          <div v-else>
+            <i>Curriculum non disponibile</i>
+          </div>
+
+          <div>
+            <label for="__services"><strong>Servizi: </strong></label>
+            <div id="__services">{{ doctor.detail.services }}</div>
+          </div>
+
+          <div>
+            <label for="__address"><strong>Indirizzo: </strong></label>
+            <div id="__address">{{ doctor.address }}</div>
+          </div>
+
+          <div>
+            <label for="__email"><strong>Email: </strong></label>
+            <div id="__email">{{ doctor.email }}</div>
+          </div>
+
+          <div class="mb-4">
+            <label for="__phone-number"><strong>Numero di telefono: </strong></label>
+            <div id="__phone-number">{{ doctor.detail.phone_number }}</div>
+          </div>
+
         </div>
-      </div>
 
-      <div>
-        <label for="__other-specs"
-          ><strong>Altre specializzazioni: </strong></label
-        >
-        <ul id="__other-specs">
-          <li v-for="spec in doctor.detail.specs">
-            {{ spec.title }}
-          </li>
-        </ul>
-      </div>
 
-      <div>
-        <label for="__description"><strong>Descrizione </strong></label>
-
-        <p id="__description">
-          {{ doctor.description }}
-        </p>
       </div>
-
-      <div v-if="doctor.detail.curriculum" class="my-4">
-        <a :href="curriculum" target="_blank" class="btn btn-primary"
-          >Vedi il CV</a
-        >
-      </div>
-      <div v-else>
-        <i>Curriculum non disponibile</i>
-      </div>
-
-      <div>
-        <label for="__services"><strong>Servizi: </strong></label>
-        <div id="__services">{{ doctor.detail.services }}</div>
-      </div>
-
-      <div>
-        <label for="__address"><strong>Indirizzo: </strong></label>
-        <div id="__address">{{ doctor.address }}</div>
-      </div>
-
-      <div>
-        <label for="__email"><strong>Email: </strong></label>
-        <div id="__email">{{ doctor.email }}</div>
-      </div>
-
-      <div class="mb-4">
-        <label for="__phone-number"
-          ><strong>Numero di telefono: </strong></label
-        >
-        <div id="__phone-number">{{ doctor.detail.phone_number }}</div>
-      </div>
-
       <hr />
 
       <!-- send a message -->
@@ -357,83 +351,38 @@ export default {
 
       <form action="" method="POST" @submit.prevent="sendMessage" class="mb-2">
         <div>
-          <label for="name" class="col-md-4 col-form-label text-md-right"
-            >Nome *</label
-          >
+          <label for="name" class="col-md-4 col-form-label text-md-right">Nome *</label>
 
           <div>
-            <input
-              id="name"
-              type="text"
-              class="form-control"
-              name="name"
-              v-model="formData.name"
-              required
-              minlength="3"
-              maxlength="50"
-              autocomplete="name"
-              autofocus
-            />
+            <input id="name" type="text" class="form-control" name="name" v-model="formData.name" required minlength="3"
+              maxlength="50" autocomplete="name" autofocus />
           </div>
         </div>
 
         <div>
-          <label for="email" class="col-md-4 col-form-label text-md-right"
-            >Email *</label
-          >
+          <label for="email" class="col-md-4 col-form-label text-md-right">Email *</label>
 
           <div>
-            <input
-              id="email"
-              type="email"
-              class="form-control"
-              name="email"
-              v-model="formData.email"
-              required
-              minlength="3"
-              maxlength="500"
-              autocomplete="email"
-              autofocus
-            />
+            <input id="email" type="email" class="form-control" name="email" v-model="formData.email" required
+              minlength="3" maxlength="500" autocomplete="email" autofocus />
           </div>
         </div>
 
         <div>
-          <label for="subject" class="col-md-4 col-form-label text-md-right"
-            >Oggetto del messaggio *</label
-          >
+          <label for="subject" class="col-md-4 col-form-label text-md-right">Oggetto del messaggio *</label>
 
           <div>
-            <input
-              id="subject"
-              type="text"
-              class="form-control"
-              name="subject"
-              v-model="formData.subject"
-              required
-              minlength="3"
-              maxlength="100"
-              autocomplete="subject"
-              autofocus
-            />
+            <input id="subject" type="text" class="form-control" name="subject" v-model="formData.subject" required
+              minlength="3" maxlength="100" autocomplete="subject" autofocus />
           </div>
         </div>
 
         <div>
-          <label for="message" class="col-md-4 col-form-label text-md-right"
-            >Messaggio *</label
-          >
+          <label for="message" class="col-md-4 col-form-label text-md-right">Messaggio *</label>
 
           <div class="mb-3">
-            <textarea
-              id="message"
-              class="form-control"
-              name="message"
-              v-model="formData.message"
-              required
-              minlength="3"
-              maxlength="500"
-            ></textarea>
+            <textarea id="message" class="form-control" name="message" v-model="formData.message" required minlength="3"
+              maxlength="500"></textarea>
           </div>
         </div>
 
@@ -461,40 +410,20 @@ export default {
 
       <form action="" method="POST" @submit.prevent="sendReview" class="mb-2">
         <div>
-          <label for="name" class="col-md-4 col-form-label text-md-right"
-            >Nome</label
-          >
+          <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
 
           <div>
-            <input
-              id="name"
-              type="text"
-              class="form-control"
-              name="name"
-              v-model="formReview.name"
-              minlength="3"
-              maxlength="50"
-              autocomplete="name"
-              autofocus
-            />
+            <input id="name" type="text" class="form-control" name="name" v-model="formReview.name" minlength="3"
+              maxlength="50" autocomplete="name" autofocus />
           </div>
         </div>
 
         <div>
-          <label for="description" class="col-md-4 col-form-label text-md-right"
-            >Recensione *</label
-          >
+          <label for="description" class="col-md-4 col-form-label text-md-right">Recensione *</label>
 
           <div class="mb-3">
-            <textarea
-              id="description"
-              class="form-control"
-              name="description"
-              v-model="formReview.description"
-              required
-              minlength="3"
-              maxlength="500"
-            ></textarea>
+            <textarea id="description" class="form-control" name="description" v-model="formReview.description" required
+              minlength="3" maxlength="500"></textarea>
           </div>
         </div>
 
@@ -515,47 +444,21 @@ export default {
       <!-- vote the doctor -->
       <h5>Vota questo dottore:</h5>
 
-      <form
-        action=""
-        method="POST"
-        @submit.prevent="sendVote"
-        class="mb-2 __main-color"
-      >
+      <form action="" method="POST" @submit.prevent="sendVote" class="mb-2 __main-color">
         <div>
-          <label for="voter" class="col-md-4 col-form-label text-md-right"
-            >Nome</label
-          >
+          <label for="voter" class="col-md-4 col-form-label text-md-right">Nome</label>
 
           <div>
-            <input
-              id="voter"
-              type="text"
-              class="form-control"
-              name="voter"
-              v-model="formVote.voter"
-              minlength="3"
-              maxlength="50"
-              autocomplete="voter"
-              autofocus
-            />
+            <input id="voter" type="text" class="form-control" name="voter" v-model="formVote.voter" minlength="3"
+              maxlength="50" autocomplete="voter" autofocus />
           </div>
         </div>
 
         <div>
-          <label
-            for="vote"
-            class="col-md-4 col-form-label text-md-right __main-color"
-            >Voto *</label
-          >
+          <label for="vote" class="col-md-4 col-form-label text-md-right __main-color">Voto *</label>
 
           <div class="mb-3">
-            <select
-              class="form-select __main-color"
-              name="vote"
-              id="vote"
-              v-model="formVote.vote"
-              required
-            >
+            <select class="form-select __main-color" name="vote" id="vote" v-model="formVote.vote" required>
               <option value="" class="__main-color" disabled selected>
                 Scegli un voto da assegnare al dottore
               </option>
@@ -593,8 +496,7 @@ export default {
           <div v-if="showMenu">
             <ul>
               <li v-for="rev in doctor.reviews">
-                <label for="__reviewer-name"
-                  ><strong>Recensore: </strong>
+                <label for="__reviewer-name"><strong>Recensore: </strong>
                 </label>
                 <div id="__reviewer-name">{{ rev.name }}</div>
                 <label for="__review-text"><strong>Testo: </strong></label>
@@ -626,10 +528,17 @@ h5,
 }
 
 hr {
-  border: none; /* Remove the default border */
-  border-top: 3px solid #0c192f; /* Set the thickness and color of the top border */
+  border: none;
+  /* Remove the default border */
+  border-top: 3px solid #0c192f;
+  /* Set the thickness and color of the top border */
 }
+
 label {
   color: #263c64;
+}
+
+#details-container {
+  max-width: 850px;
 }
 </style>
